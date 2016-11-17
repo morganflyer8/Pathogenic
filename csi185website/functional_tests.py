1244c0bb69b1e5cce907dc746c37509fe02acbaf
Morgan Flyer <MorganFlyer@Stevens-MacBook-Pro-3.local>
2016-11-16 19:22:03 -0500

    saving nov 16

--- csi185website/functional_tests.py
+++ csi185website/functional_tests.py
@@ -29,7 +29,10 @@ class NewVisitorTest(unittest.TestCase):
     def test_home_page(self):
         """
 
-        Put your description of the purpose of your web site here.
+     When young adolescent women subscribe to culturally determined fashion expectations, they run the risk of succumbing to pathogenic dieting methods.
+     In the early part of the 21st century, the photographs, stories, and symbols reinforced through advertising, social media, television and film promote the concept of thinness.
+     This trope affects a women’s medical wellbeing and can lead to pathogenic eating disorder, most notably anorexia.
+     This project examines the triggers within contemporary fashion that de-stabilizes the self-perception of young adolescent women.
 
         """
 
@@ -38,14 +41,14 @@ class NewVisitorTest(unittest.TestCase):
         # there is a page title defined by <title></title> on the home page
         # check it
 
-        self.assertIn('Pathogenic Fashion',self.browser.title)
+        self.assertIn("Pathogenic Fashion",self.browser.title)
 
         # You will have an image for your home page I am assuming.
         # Put the name of your image here in place of homebrew.png
         # In general this is how we check for images on a page.
 
         m=self.browser.find_element_by_tag_name('img')
-        self.assertIn('homebrew.png',m.get_attribute('src'))
+        self.assertIn('model.jpg',m.get_attribute('src'))
 
         # We check here for the title of your home page.
         # uncomment the next lines and change the text when you set your title.
@@ -57,20 +60,11 @@ class NewVisitorTest(unittest.TestCase):
         # There is an area specified around the computer keyboard.
         # the 'id' of this area is 'keyboard'
 
-        # this is how we find that area.
-        a=self.browser.find_element_by_id('keyboard')
-
-        # this is how we click on it.
+        a=self.browser.find_element_by_id('lower')
         a.click()
 
-        # after clicking on it, we should see the next page.
-        e=self.browser.find_element_by_css_selector("h1")
-        self.assertIn('Keyboards',e.text)
-
-        # The page should have a picture of a teletype machine. 
-        m=self.browser.find_element_by_tag_name('img')
-        self.assertIn('teletype.jpg',m.get_attribute('src'))
-    
+        h=self.browser.find_element_by_css_selector('h1')
+        
 
 if __name__=="__main__":
         unittest.main(warnings="ignore")
